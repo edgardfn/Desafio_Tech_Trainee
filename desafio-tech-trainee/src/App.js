d52abf4257118b6273e90abd3bd1865dbca586bb
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-10 19:03:13 -0300

    Criado estado do componente Principal e funcao de pegar Posicao.

--- desafio-tech-trainee/src/App.js
+++ desafio-tech-trainee/src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import { Principal } from './screens/Principal/Principal'
 
 function App() {
   return (
     <div className="App">
-
+      <Principal />
     </div>
   );
 }
--- desafio-tech-trainee/src/screens/Principal/Principal.js
+++ desafio-tech-trainee/src/screens/Principal/Principal.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { DivisaoPrincipal } from './stylesPrincipal'
 
 export function Principal() {
+    const [ posicao, setPosicao ] = useState({})
+
+    useEffect(() => {
+
+        pegarPosicao()
+
+    }, [])
+
+    const pegarPosicao = () => {
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((posicao) => {
+                setPosicao(posicao.coords)
+            });
+          } else {
+            alert("GeoLocalização não está ativa ou não é suportada por este navegador.")
+          }
+
+    }
 
 
-    return <div>
+    return <DivisaoPrincipal>
         
-    </div>
+    </DivisaoPrincipal>
 
 
 }
