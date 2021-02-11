508de95623c4578e09a6d7e66eb62f47514a048f
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-11 17:32:36 -0300

    Feito logica para ler do estado se está claro ou não e exibir fundo da tela de acordo com essa informação.

--- desafio-tech-trainee/src/screens/Principal/Principal.js
+++ desafio-tech-trainee/src/screens/Principal/Principal.js
@@ -2,28 +2,42 @@ import React, { useEffect, useState } from 'react'
 import { DivisaoPrincipal } from './stylesPrincipal'
 import axios from 'axios'
 import { BASE_URL } from '../../constants/requisicoes'
+import ceuAzul from '../../assets/ceu-azul.jpg'
 
 export function Principal() {
     const [ posicao, setPosicao ] = useState({})
     const [ nascerDoSol, setNascerDoSol ] = useState([])
     const [ porDoSol, setPorDoSol ] = useState([])
     const [ horaAtual, setHoraAtual ] = useState([])
+    const [ luzDoDia, setLuzDoDia ] = useState(true) 
     
 
     useEffect(() => {
         
+        pegarPosicaoUsuario()
+
+        // if (navigator.geolocation) {
+        //     navigator.geolocation.getCurrentPosition((position) => {
+        //         setPosicao(position.coords)
+        //     });
+        // } else {
+        //     alert("GeoLocalização não está ativa ou não é suportada por este navegador.")
+        // }
+
+    }, [])
+
+    const pegarPosicaoUsuario = () => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 setPosicao(position.coords)
+                pegarDadosNascerEPorDoSol()
             });
         } else {
             alert("GeoLocalização não está ativa ou não é suportada por este navegador.")
         }
+    }
 
-    }, [setPosicao])
-
-    useEffect(() => {
-
+    const pegarDadosNascerEPorDoSol = () => {
         if(posicao.latitude !== undefined && posicao.longitude !== undefined) {
             axios.get(`${BASE_URL}?lat=${posicao.latitude}&lng=${posicao.longitude}`).then(resposta => {
                 let arrayNascerDoSol = resposta.data.results.sunrise.split(":")
@@ -45,8 +59,33 @@ export function Principal() {
                 alert(erro.message);
             })
         }
+    }
 
-    }, [posicao])
+    // useEffect(() => {
+
+    //     if(posicao.latitude !== undefined && posicao.longitude !== undefined) {
+    //         axios.get(`${BASE_URL}?lat=${posicao.latitude}&lng=${posicao.longitude}`).then(resposta => {
+    //             let arrayNascerDoSol = resposta.data.results.sunrise.split(":")
+    //             arrayNascerDoSol[0] = Number(arrayNascerDoSol[0]) - 3;
+    //             arrayNascerDoSol[1] = Number(arrayNascerDoSol[1])
+                
+    //             let arrayPorDoSol = resposta.data.results.sunset.split(":")
+    //             arrayPorDoSol[0] = (Number(arrayPorDoSol[0]) + 12) - 3;
+    //             arrayPorDoSol[1] = Number(arrayPorDoSol[1])
+
+    //             setNascerDoSol(arrayNascerDoSol)
+    //             setPorDoSol(arrayPorDoSol)
+
+    //             pegarDataEHora()
+
+                
+
+    //         }).catch( erro => {
+    //             alert(erro.message);
+    //         })
+    //     }
+
+    // }, [posicao])
 
     const pegarDataEHora  = () => {
         const dataEHora = Date()
@@ -61,33 +100,53 @@ export function Principal() {
     useEffect(() => {
         if(nascerDoSol.length > 0 && porDoSol.length > 0 && horaAtual.length > 0) {
            if(horaAtual[0] > nascerDoSol[0] && horaAtual[0] < porDoSol[0]) {
-               console.log("Sol(dia)")
+            setLuzDoDia(true) 
            } else if(horaAtual[0] === nascerDoSol[0]) {
                if(horaAtual[1] >= nascerDoSol[1] ) {
-                console.log("Sol(dia)")
+                setLuzDoDia(true) 
                } else {
-                console.log("Noite")
+                setLuzDoDia(false) 
                }
            } else if(horaAtual[0] === porDoSol[0]) {
                if(horaAtual[1] >= porDoSol[1]) {
-                console.log("Noite")
+                setLuzDoDia(false)
                } else {
-                console.log("Sol(dia)")
+                setLuzDoDia(false)  
                }
            } else {
-            console.log("Noite")
+            setLuzDoDia(false) 
            }
         }
         
     }, [horaAtual,nascerDoSol, porDoSol ])
     
 
-    
+    // if(nascerDoSol.length > 0 && porDoSol.length > 0 && horaAtual.length > 0) {
+    //     if(horaAtual[0] > nascerDoSol[0] && horaAtual[0] < porDoSol[0]) {
+    //      setLuzDoDia(true) 
+    //     } else if(horaAtual[0] === nascerDoSol[0]) {
+    //         if(horaAtual[1] >= nascerDoSol[1] ) {
+    //          setLuzDoDia(true) 
+    //         } else {
+    //          setLuzDoDia(false) 
+    //         }
+    //     } else if(horaAtual[0] === porDoSol[0]) {
+    //         if(horaAtual[1] >= porDoSol[1]) {
+    //          setLuzDoDia(false)
+    //         } else {
+    //          setLuzDoDia(false)  
+    //         }
+    //     } else {
+    //      setLuzDoDia(false) 
+    //     }
+    // }
 
     
-    return <DivisaoPrincipal>
+    
+    return <DivisaoPrincipal luz={luzDoDia} >
         
     </DivisaoPrincipal>
 
 
 }
+
--- desafio-tech-trainee/src/screens/Principal/stylesPrincipal.js
+++ desafio-tech-trainee/src/screens/Principal/stylesPrincipal.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components'
+import ceuAzul from '../../assets/ceu-azul.jpg'
+import ceuNoite from '../../assets/ceu-noite.jpg'
 
 export const DivisaoPrincipal = styled.div`
+    width: 100vw;
+    height: 100vh;
+    
+    background-image: ${(props) => {
+        if(props.luz === true) {
+            return `url(${ceuAzul})`
+        } else {
+            return  `url(${ceuNoite})`
+        }
+    }};
 
-`
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+`   
