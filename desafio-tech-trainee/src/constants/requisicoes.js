64e677fa7616b6cb255fc9ba6762bb98a2038086
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-10 23:05:59 -0300

    Criado funcao para gerar requisicao para Api para pegar dados da luz solar, criado arquivo de constantes das requisicoes e instalado dependencia axios.

---
+++ desafio-tech-trainee/src/constants/requisicoes.js
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  "https://api.sunrise-sunset.org/json";
--- desafio-tech-trainee/src/screens/Principal/Principal.js
+++ desafio-tech-trainee/src/screens/Principal/Principal.js
@@ -5,24 +5,47 @@ export function Principal() {
     const [ posicao, setPosicao ] = useState({})
 
     useEffect(() => {
-
-        pegarPosicao()
-
-    }, [])
-
-    const pegarPosicao = () => {
-
+        
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((posicao) => {
-                setPosicao(posicao.coords)
+            navigator.geolocation.getCurrentPosition((position) => {
+                setPosicao(position.coords)
             });
-          } else {
+        } else {
             alert("GeoLocalização não está ativa ou não é suportada por este navegador.")
-          }
+        }
 
-    }
+    }, [setPosicao])
 
+    useEffect(() => {
+        pegarDadosLuz()
+    }, [posicao])
 
+    const pegarDadosLuz = async () => {
+        console.log(posicao.latitude, posicao.longitude)
+        try {
+          const resposta = await axios.get(`${BASE_URL}?lat=${posicao.latitude}&lng=${posicao.longitude}`);
+    
+          this.setState({ usuarios: resposta.data });
+        } catch (erro) {
+          alert(erro.message);
+        }
+    };
+
+    // const pegarPosicao = () => {
+
+    //     if (navigator.geolocation) {
+    //         navigator.geolocation.getCurrentPosition((position) => {
+    //             setPosicao(position.coords)
+    //         });
+    //     } else {
+    //         alert("GeoLocalização não está ativa ou não é suportada por este navegador.")
+    //     }
+
+    // }
+
+    
+
+    
     return <DivisaoPrincipal>
         
     </DivisaoPrincipal>
